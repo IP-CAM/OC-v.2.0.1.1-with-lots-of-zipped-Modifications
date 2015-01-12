58d583e2a410d2f18fae955f6a6d2b5d0eeb3c07
nothingtrue <mkh_eg@hotmail.com>
2015-01-12 15:42:03 +0200

    start fresh ver oc 2.0.1.1

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/oc2/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/oc2/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\wamp\www\oc2/catalog/');
+define('DIR_SYSTEM', 'D:\wamp\www\oc2/system/');
+define('DIR_LANGUAGE', 'D:\wamp\www\oc2/catalog/language/');
+define('DIR_TEMPLATE', 'D:\wamp\www\oc2/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\wamp\www\oc2/system/config/');
+define('DIR_IMAGE', 'D:\wamp\www\oc2/image/');
+define('DIR_CACHE', 'D:\wamp\www\oc2/system/cache/');
+define('DIR_DOWNLOAD', 'D:\wamp\www\oc2/system/download/');
+define('DIR_UPLOAD', 'D:\wamp\www\oc2/system/upload/');
+define('DIR_MODIFICATION', 'D:\wamp\www\oc2/system/modification/');
+define('DIR_LOGS', 'D:\wamp\www\oc2/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'oc2');
+define('DB_PREFIX', '');
